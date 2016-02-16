5d19e4ebaae5aa4dd1ec7711e14a80bf53add1c5
Otávio Vicente Ribeiro <otavio.ribeiro@freire.com.br>
2016-02-16 13:03:36 -0300

    Lista de tokens atualizada

---
+++ src/lexer/definitions/tToken.java
@@ -0,0 +1,34 @@
+package lexer.definitions;
+
+public class tToken {
+	private String tokenClass;
+	private String tokenValue;
+	private int tokenClassIndex; 
+	
+	public tToken(String tokenClass){
+		this.tokenClass = tokenClass;
+	}
+	
+	public tToken(String tokenClass, String tokenValue) {
+		//this.tokenClass = tokenClass;
+		this(tokenClass);
+		this.tokenValue = tokenValue;
+	}
+	
+	public tToken(String tokenClass, String tokenValue, int tokenClassIndex) {
+		this(tokenClass, tokenValue);
+		this.tokenClassIndex = tokenClassIndex;
+	}
+	
+	public String getToken() {
+		return ("<" + this.tokenClass + ", " + this.tokenValue + ">");
+	}
+	
+	public int getTokenClassIndex(){
+		return this.tokenClassIndex;
+	}
+	
+	public void setTokenValue(String tokenValue){
+		this.tokenValue = tokenValue;
+	}
+}
