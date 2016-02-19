ccc0debcee4f90fadf11cc45e0e086d79f99d757
Otávio Gonçalvez Vicente Ribeiro Filho <Otvio@172.20.205.226>
2016-02-18 21:28:18 -0300

    Iniciando a condificação dos estados

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -5,4 +5,8 @@ import lexer.definitions.Tokens;
 public class Lexer {
 	private Tokens tokens;
 	
+	int edges[][] = {/* 0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,+,-,*,<,>,=,!,(,),[,],{,},&,|*/
+			           {}
+	};
+	
 }
