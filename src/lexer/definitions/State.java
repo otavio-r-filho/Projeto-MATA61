e638835107a88892104c5fa585ebf0754c17bd14
Otávio Vicente Ribeiro <otavio.ribeiro@freire.com.br>
2016-02-19 11:28:44 -0300

    Atualizando lista de Tokens Atualizando classe de estado

--- src/lexer/definitions/State.java
+++ src/lexer/definitions/State.java
@@ -1,19 +1,32 @@
 package lexer.definitions;
 
-import lexer.definitions.Tokens;
+import lexer.definitions.tToken;
 
 public class State {
 	private boolean finalState;
 	private int stateIndex;
 	private String stateName;
 	private tToken returnToken;
+	private String returnValue;
 	
 	
-	public State(int stateIndex, String stateName, boolean finalState, tToken returnToken) {
+	public State(int stateIndex, String stateName) {
 		this.stateIndex = stateIndex;
 		this.stateName = stateName;
+	}
+	
+	public State(int stateIndex, String stateName, boolean finalState) {
+		this(stateIndex, stateName);
 		this.finalState = finalState;
+	}
+	
+	public State(int stateIndex, String stateName, boolean finalState, tToken returnToken) {
+		this(stateIndex, stateName, finalState);
 		this.returnToken = returnToken;		
+	}
+	
+	public boolean isFinalState() {
+		return this.finalState;
 	}
 	
 }
--- src/lexer/definitions/Tokens.java
+++ src/lexer/definitions/Tokens.java
@@ -4,44 +4,44 @@ import lexer.definitions.tToken;
 
 public class Tokens {
 	//Lexemas reservados - comandos
-	public static tToken ELSE = new tToken("ELSE", "else", 1);
-	public static tToken FLOAT = new tToken("FLOAT", "float", 2);
-	public static tToken FOR = new tToken("FOR", "for", 3);
-	public static tToken IF = new tToken("IF", "if", 4);
-	public static tToken INT = new tToken("INTEGER", "int", 5);
-	public static tToken NEW = new tToken("NEW", "new", 6);
-	public static tToken RETURN = new tToken("RETURN", "return", 7);
-	public static tToken STR = new tToken("STRING", "str", 8);
-	public static tToken THEN = new tToken("THEN", "then", 9);
-	public static tToken VOID = new tToken("VOID", "void", 10);
-	public static tToken WHILE = new tToken("WHILE", "while", 11);
+	public static final tToken ELSE = new tToken("ELSE", "else", 1);
+	public static final tToken FLOAT = new tToken("FLOAT", "float", 2);
+	public static final tToken FOR = new tToken("FOR", "for", 3);
+	public static final tToken IF = new tToken("IF", "if", 4);
+	public static final tToken INT = new tToken("INTEGER", "int", 5);
+	public static final tToken NEW = new tToken("NEW", "new", 6);
+	public static final tToken RETURN = new tToken("RETURN", "return", 7);
+	public static final tToken STR = new tToken("STRING", "str", 8);
+	public static final tToken THEN = new tToken("THEN", "then", 9);
+	public static final tToken VOID = new tToken("VOID", "void", 10);
+	public static final tToken WHILE = new tToken("WHILE", "while", 11);
 	
 	
 	//Lexemas reservados - caracteres
-	public static tToken PLUS = new tToken("PLUS", "+", 12);
-	public static tToken MINUS = new tToken("MINUS", "-", 13);
-	public static tToken ASTERISK = new tToken("ASTERISK", "*", 14);
-	public static tToken SLASH = new tToken("SLASH", "/", 15);
-	public static tToken SMALLER = new tToken("SMALLER", "<", 16);
-	public static tToken GRATER = new tToken("GREATER", ">", 17);
-	public static tToken ATTRIBUTION = new tToken("ATTRIBUTION", "=", 18);
-	public static tToken SEQUAL = new tToken("SQUAL", "<=", 19);
-	public static tToken GEQUAL = new tToken("GEQUAL", ">=", 20);
-	public static tToken COMPARISSON = new tToken("COMPARISSON", "==", 21);
-	public static tToken DIFFERENT = new tToken("DIFFERENT", "!=", 22);
-	public static tToken SEMICOLON = new tToken("SEMICOLON", ";", 23);
-	public static tToken COMMA = new tToken("COMMA", ",", 24);
-	public static tToken OPARENTHESES = new tToken("OPARENTHESES", "(", 25);
-	public static tToken CPARENTHESES = new tToken("CPARENTHESES", ")", 26);
-	public static tToken OBRACKET = new tToken("OBRACKET", "[", 27);
-	public static tToken CBRACKET = new tToken("CBRACKET", "]", 28);
-	public static tToken OKEYBRACKET = new tToken("OKEYBRACKET", "{", 29);
-	public static tToken CKEYBRACKET = new tToken("CKEYBRACKET", "}", 30);
-	public static tToken COMERCIALE = new tToken("COMERCIALE", "&", 31);
-	public static tToken PIPE = new tToken("PIPE", "|", 32);
+	public static final tToken PLUS = new tToken("PLUS", "+", 12);
+	public static final tToken MINUS = new tToken("MINUS", "-", 13);
+	public static final tToken ASTERISK = new tToken("ASTERISK", "*", 14);
+	public static final tToken SLASH = new tToken("SLASH", "/", 15);
+	public static final tToken SMALLER = new tToken("SMALLER", "<", 16);
+	public static final tToken GRATER = new tToken("GREATER", ">", 17);
+	public static final tToken ATTRIBUTION = new tToken("ATTRIBUTION", "=", 18);
+	public static final tToken SEQUAL = new tToken("SQUAL", "<=", 19);
+	public static final tToken GEQUAL = new tToken("GEQUAL", ">=", 20);
+	public static final tToken COMPARISSON = new tToken("COMPARISSON", "==", 21);
+	public static final tToken DIFFERENT = new tToken("DIFFERENT", "!=", 22);
+	public static final tToken SEMICOLON = new tToken("SEMICOLON", ";", 23);
+	public static final tToken COMMA = new tToken("COMMA", ",", 24);
+	public static final tToken OPARENTHESES = new tToken("OPARENTHESES", "(", 25);
+	public static final tToken CPARENTHESES = new tToken("CPARENTHESES", ")", 26);
+	public static final tToken OBRACKET = new tToken("OBRACKET", "[", 27);
+	public static final tToken CBRACKET = new tToken("CBRACKET", "]", 28);
+	public static final tToken OKEYBRACKET = new tToken("OKEYBRACKET", "{", 29);
+	public static final tToken CKEYBRACKET = new tToken("CKEYBRACKET", "}", 30);
+	public static final tToken COMERCIALE = new tToken("COMERCIALE", "&", 31);
+	public static final tToken PIPE = new tToken("PIPE", "|", 32);
 	
 	//Lexemas "abertos"
-	public static tToken ID = new tToken("ID", "", 33);
-	public static tToken NUM = new tToken("NUM", "", 34);
-	public static tToken REAL = new tToken("REAL", "", 35);
+	public static final tToken ID = new tToken("ID", "", 33);
+	public static final tToken NUM = new tToken("NUM", "", 34);
+	public static final tToken REAL = new tToken("REAL", "", 35);
 }
