f0a62a29098d8de0fe4e4f039afa146b3d62a77b
Otávio Gonçalvez Vicente Ribeiro Filho <Otvio@172.20.205.226>
2016-03-02 05:19:18 -0300

    Atualizando.

--- src/lexer/definitions/Lexemes.java
+++ src/lexer/definitions/Lexemes.java
@@ -95,4 +95,36 @@ public class Lexemes {
 	public static final Lexeme POINT 		= new Lexeme(76, ".");
 	public static final Lexeme SLASH 		= new Lexeme(77,"/");
 	
+	public static int getLexemeIndex(String lexeme){
+		
+		switch(lexeme) {
+		case "0":
+			return 0;
+		case "1":
+			return 1;
+		case "2":
+			return 2;
+		case "3":
+			return 3;
+		case "4":
+			return 4;
+		case "5":
+			return 5;
+		case "6":
+			return 6;
+		case "7":
+			return 7;
+		case "8":
+			return 8;
+		case "9":
+			return 9;
+		case "A":
+			return 10;
+		case "B":
+			return 11;
+			
+		}
+		return -1;
+	}
+	
 }
