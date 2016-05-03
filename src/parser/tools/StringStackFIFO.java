319cc6953959a7181e4eb88d8b2e2aad45a072d2
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-05-03 12:21:19 -0400

    Adicionando StringStackFIFO

---
+++ src/parser/tools/StringStackFIFO.java
@@ -0,0 +1,82 @@
+package parser.tools;
+
+import java.util.ArrayList;
+
+public class StringStackFIFO {
+	
+	private ArrayList<String> stringList;
+	private int operationMode;
+	
+	public StringStackFIFO(int operationMode) {
+		this.operationMode = operationMode;
+		this.stringList = new ArrayList<String>();
+	}
+	
+	public void push(String word) {
+		stringList.add(word);
+	}
+	
+	public String pop() {
+		if(!stringList.isEmpty()) {
+			if(operationMode == 1) {
+				return popStack();
+			}
+			return popFIFO();
+		}
+		return null;
+	}
+	
+	private String popStack() {
+		String popResult = stringList.get(stringList.size() - 1);
+		stringList.remove(stringList.size() - 1);
+		return popResult;
+	}
+	
+	private String popFIFO() {
+		String popResult = stringList.get(0);
+		stringList.remove(0);
+		return popResult;
+	}
+	
+	public String checkTop() {
+		if(!stringList.isEmpty()) {
+			if(operationMode == 1) {
+				return checkTopStack();
+			}
+			return checkTopFIFO();
+		}
+		return null;
+	}
+	
+	private String checkTopStack() {
+		return stringList.get(stringList.size() - 1);
+	}
+	
+	private String checkTopFIFO() {
+		return stringList.get(0);
+	}
+	
+	public String operationMode() {
+		if(operationMode == 1) {
+			return "Stack";
+		}
+		
+		return "FIFO";
+	}
+	
+	public void setOperationMode(String operationMode) {
+		if(operationMode.equals("Stack")) {
+			this.operationMode = 1;
+			return;
+		}
+		this.operationMode = 2;
+	}
+	
+	public void setOperationMode(int operationMode) {
+		if(operationMode == 1) {
+			this.operationMode = 1;
+			return;
+		}
+		this.operationMode = 2;
+	}
+}
