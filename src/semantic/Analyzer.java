72f834eaeb79b88eddf789c2d095366c103d4745
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-05-20 14:55:04 -0300

    Atualizando

--- src/semantic/Analyzer.java
+++ src/semantic/Analyzer.java
@@ -318,7 +318,7 @@ public class Analyzer {
         }
         return "ERROR";
     }
-    
+
     public boolean checkDeclaration(ASTNode node) {
         CallExpression callExpression;
         AttributionNode attributionNode;
